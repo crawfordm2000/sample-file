9c70789c931ad612e9eba1f233e6765bcc0425b3
Matthew Crawford <crawfordm2000@gmail.com>
2020-03-23 20:21:33 -0400

    Removed some code

--- test.py
+++ test.py
@@ -4,18 +4,8 @@ from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 
-
 print("Success:", r.status_code)
-
-print("Success:", r.status_code)
+print(r.ok)
